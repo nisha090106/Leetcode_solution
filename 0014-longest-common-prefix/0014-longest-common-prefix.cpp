ca462bde51bcfd953efe4c9af25b67a6669eb598
Nisha Pragane <nisha.1252090013@vit.edu>
2026-09-10 00:03:15 +0530

    Time: 0 ms (100%), Space: 12 MB (39.33%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,8 +3,8 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string prefix = strs[0];
 
-        for(int i =0;i<strs.size();i++){
-            int j =0;
+        for(int i =1;i<strs.size();i++){
+            int j=0;
             while(j<prefix.size()&& j<strs[i].size() && prefix[j]==strs[i][j]){
                 j++;
             }
